1fa5c948caa9aeaf0ef142cd798433f8acf6ae37
Lane Wright <lane.wright@rocketmail.com>
2013-05-08 19:05:42 -0400

    Added packages com.beastlypen9uin.CouchMod and com.beastlypen9uin.CouchMod.lib aswell as added core class and reference variable class.

---
+++ couch_common/com/beastlypen9uin/couchmod/lib/Reference.java
@@ -0,0 +1,9 @@
+package com.beastlypen9uin.couchmod.lib;
+
+public class Reference {
+    
+    public static final String MOD_ID = "BPCM";
+    public static final String MOD_NAME = "BeastlyPen9un's Couch Mod";
+    public static final String VERSION = "pre0.1";
+
+}
