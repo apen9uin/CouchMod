301b5a97a223decae9e8debddb6b0b5b25c6fb29
Lane Wright <lane.wright@rocketmail.com>
2013-07-31 14:16:38 -0400

    Clean up a little. ~ a_pen9uin

--- reference/CouchModVars.java
+++ reference/CouchModVars.java
@@ -2,22 +2,25 @@ package CouchMod.reference;
 
 public class CouchModVars 
 {
-	// add in version info and globally used variables here
+	//General Mod info
 	
 	public static final String MODID                   = "couchmod";
 	public static final String MODNAME                 = "Beastlypen9uin's Couch Mod";
 	public static final String VERSION                 = "concept";
 	public static final String CHANNEL                 = "couchChannel";
 	
+	//Names for items
 	public static final String COUCHLEATHERUNLOCALNAME = "couchLeather";
 	public static final String COCUHLEATHERLOCALNAME   = "Couch Leather";
 	public static final String COUCHUNLOCALNAME        = "couchItem";
 	public static final String COUCHLOCALNAME          = "Couch";
 	
+	//Textures
 	public static final String TEXTURES_LOCATION       = "couchmod";
 	public static final String COUCHLEATHERICON        = "couchLeatherIcon";
 	public static final String COUCHITEM               = "couchItemIcon";
 	
+	//Item and Block IDs
 	public static       int    COUCHLEATHERID;
 	public static       int    COUCHITEMID;
 	public static       int    COUCHBLOCKID;
